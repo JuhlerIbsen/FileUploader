c66e67229db05c87110b023385f0cc12fa81bc71
Michael Ibsen <michael_c_ibsen@hotmail.com>
2017-06-05 22:35:40 +0200

    Initial files.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+
+<body>
+
+<form action="upload-media" method="post">
+    <label for="title">Media Title</label>
+    <input type="text" name="title" id="title"/>
+
+    <label for="upload">Upload file</label><
+    <input type="file" name="upload" id="upload"/>
+
+    <input type="submit" name="btnSubmit" value="Upload file!" />
+</form>
+
+</body>
+</html>
