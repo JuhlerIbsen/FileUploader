89d80d97f6e6874187755422d071c0c6a7d6f10d
Michael Ibsen <michael_c_ibsen@hotmail.com>
2017-06-05 23:43:19 +0200

    Show a list of links with all uploaded items. :smile:

--- index.php
+++ index.php
@@ -12,8 +12,37 @@
     <label for="upload">Upload file</label>
     <input type="file" name="upload" id="upload"/>
 
-    <input type="submit" name="btnSubmit" value="Upload file!" />
+    <input type="submit" name="btnSubmit" value="Upload file!"/>
 </form>
+
+<br>
+
+<?php include 'config/config.php';
+
+$query = "SELECT * FROM media;";
+
+// Connect to database.
+$conn = connect();
+
+// Initialize statement.
+$stmt = mysqli_stmt_init($conn);
+
+// Prepare statement.
+mysqli_stmt_prepare($stmt, $query);
+
+// Execute statement.
+mysqli_stmt_execute($stmt);
+
+// Bind columns.
+mysqli_stmt_bind_result($stmt, $id, $title, $destination, $ip);
+
+// Fetch data from bind_result.
+while (mysqli_stmt_fetch($stmt)) {
+    echo "<a href='" . $destination . "' target='_blank'>$title</a> - uploaded by " . $ip . "<br>";
+}
+
+
+?>
 
 </body>
 </html>
